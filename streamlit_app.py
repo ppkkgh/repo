1ce16027ad8378e12d9daa5218bdd21f929f0448
ppkkgh <ppkkgm@gmail.com>
2024-03-19 10:58:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ def test(url):
     # url = "https://google.com"
 
     # Make a GET request to the API
+    st.write(url)
     response = requests.get(url)
 
     # Check if the request was successful
